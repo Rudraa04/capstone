20d1db5afd3c6d01110ea4ad37ef8fd54f4a8618
Harshil2103 <harshilvijaykumar.rathod@edu.sait.ca>
2025-07-12 23:55:50 -0600

    updates

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+//lets us move the user to another page
 import { Navigate } from "react-router-dom";
 
+// This is our special ProtectedRoute component.
+// We give it:
+// 1. userRole → who the user is (like "admin", "customer")
+// 2. allowedRoles → who is allowed to go in (like only "admin")
+// 3. children → the actual page we want to show if allowed
 export default function ProtectedRoute({ userRole, allowedRoles, children }) {
+  // If no userRole (means user is NOT logged in), we send them to the login page.
+  if (userRole === null) {
+    return <Navigate to="/login" replace />;
+  }
+
+  //  If the user's role is NOT in the list of allowedRoles (like not an admin), 
+  // we send them back to the homepage (they're not allowed to see this page).
   if (!allowedRoles.includes(userRole)) {
     return <Navigate to="/" replace />;
   }
+//If the user is logged in AND has permission, 
+  // we show them the page they are trying to go to.
   return children;
 }
