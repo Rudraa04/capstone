ad243f9f120e90382b30403f6857548252082bf3
Harshil2103 <harshilvijaykumar.rathod@edu.sait.ca>
2025-07-18 14:47:08 -0600

    updates

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 // express import to create router
+//
 import express from "express";
 //all controller logic for handling product logic
 import {
-  getProductsByType, //fetching all items of a specific product type
+  getProductsByType, //fetching all items of a specific product type , comes from controller
   getProductById, //fetching a single product by id and type
   addProduct, // creating a new product for a given type
   updateProduct, // updating product for a given type and id
@@ -15,7 +16,7 @@ const router = express.Router();
 // Get all products combined /api/products/all
 router.get("/api/products/all", getAllProducts);
 
-// route to fetch all products of a specific type /api/products/tiles
+// route to fetch all products of a specific type /api/products/tiles from mongodb
 router.get("/api/products/:type", getProductsByType);
 //fetch a single product of a specific type /api/products/tiles/01234
 router.get("/api/products/:type/:id", getProductById);
