de903cc7f437360f315f0c5bbad8853fe26779ca
Rudraa04 <167252539+Rudraa04@users.noreply.github.com>
2025-08-12 16:45:20 -0600

    update

--- src/admin/TilesInventory.jsx
+++ src/admin/TilesInventory.jsx
@@ -665,7 +665,7 @@ export default function TilesInventory() {
                 type="file"
                 accept="image/*"
                 onChange={handleImageChange}
-                required
+              
                 className="block"
               />
               {image && (
